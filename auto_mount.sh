c4c1beb0a509387d4dabec2e7cbdc536520ed8c2
Aaron Honeycutt <aaronhoneycutt@proton.me>
2023-04-18 10:31:47 -0600

    Work on cleaning it up and automation

--- auto_mount.sh
+++ auto_mount.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# Intro
-# -------------
+
+# Set append for drive automation
+APPEND=""
+
 clear
 echo "-------------------------------------------------------------------------------"
 echo " A tool to mount an installed OS from a live disk for repair or data recovery |" 
@@ -9,23 +11,33 @@ echo "--------------------------------------------------------------------------
 sleep 2
 clear
 
-# Setting username for OS logic
 echo "-------------------------------------------------------------------------------"
-lsblk
+lsblk -f
 echo "-------------------------------------------------------------------------------"
-
-echo ""
 
 read -p 'Which drive has your OS? ' drivevar
 
-echo ""
-echo "NOTE: For Ubuntu is would be the 2nd and for Pop!_OS it would be the 3rd"
+if [[ "$drivevar" == "/dev/nvme"* || "$drivevar" == "/dev/mmcblk0"* ]]; then
+  APPEND="p"
+fi
 
-read -p 'What is the root partition? ' rootvar
+efiName=${driveName}$APPEND
+efiName+=1
+rootName=${driveName}$APPEND
+rootName+=3
+swapName=${driveName}$APPEND
+swapName+=4
 
+if [[ $distro = "Pop!_OS 22.04 LTS" ]]; then
+   echo "I am Pop"
+   echo "Your EFI partition is" $efiName
+   echo "Your root partition is" $rootName
+fi
+
+#read -p 'What is the root partition? ' rootvar
 #read -p 'What is the EFI partition? ' efivar
-efivar=$(lsblk -if | grep EFI | awk -F- {'print$2'}  | awk {'print "/dev/"$1'})
-read -p 'What is your OS? ' osvar
+#efivar=$(lsblk -if | grep EFI | awk -F- {'print$2'}  | awk {'print "/dev/"$1'})
+
 read -p 'Is your drive encrypted? ' encryptvar
 
 ### Pop section
@@ -65,116 +77,14 @@ if [[ $drivevar = nvme && $osvar = pop && $encryptvar = yes ]]
             sudo cp /etc/resolv.conf /mnt/etc/
             sudo chroot /mnt
 ​
-### Ubuntu section
-​
-    elif [[ $drivevar = nvme && $osvar = ubuntu && $encryptvar = yes ]]
-        then
-            sudo cryptsetup luksOpen $rootvar cryptdata
-            sudo lvscan
-            sudo vgchange -ay
-            sudo mount /dev/mapper/vgubuntu-root /mnt
-            sudo mount $efivar /mnt/boot/efi
-            for i in /dev /dev/pts /proc /sys /run; do sudo mount -B $i /mnt$i; done
-            sudo cp /etc/resolv.conf /mnt/etc/
-            sudo chroot /mnt
-    elif [[ $drivevar = sata && $osvar = ubuntu && $encryptvar = yes ]]
-        then
-            sudo cryptsetup luksOpen /dev/$rootvar cryptdata
-            sudo lvscan
-            sudo vgchange -ay
-            sudo mount /dev/mapper/vgubuntu-root /mnt
-            sudo mount $efivar /mnt/boot/efi
-            for i in /dev /dev/pts /proc /sys /run; do sudo mount -B $i /mnt$i; done
-            sudo cp /etc/resolv.conf /mnt/etc/
-            sudo chroot /mnt
-    elif [[ $drivevar = nvme && $osvar = ubuntu && $encryptvar = no ]]
-        then
-            sudo mount $rootvar /mnt
-            sudo mount $efivar /mnt/boot/efi
-            for i in /dev /dev/pts /proc /sys /run; do sudo mount -B $i /mnt$i; done
-            sudo cp /etc/resolv.conf /mnt/etc/
-            sudo chroot /mnt
-    elif [[ $drivevar = sata && $osvar = ubuntu && $encryptvar = no ]]
-        then
-            sudo mount $rootvar /mnt
-            sudo mount $efivar /mnt/boot/efi
-            for i in /dev /dev/pts /proc /sys /run; do sudo mount -B $i /mnt$i; done
-            sudo cp /etc/resolv.conf /mnt/etc/
-            sudo chroot /mnt
+# Ubuntu section
+## sudo mount /dev/mapper/vgubuntu-root /mnt
 
-### Fedora section
+# Fedora section
+## sudo mount /dev/mapper/vgubuntu-root /mnt
 
-    elif [[ $drivevar = nvme && $osvar = fedora && $encryptvar = yes ]]
-        then
-            sudo cryptsetup luksOpen $rootvar cryptdata
-            sudo lvscan
-            sudo vgchange -ay
-            sudo mount /dev/mapper/vgubuntu-root /mnt
-            sudo mount $efivar /mnt/boot/efi
-            for i in /dev /dev/pts /proc /sys /run; do sudo mount -B $i /mnt$i; done
-            sudo cp /etc/resolv.conf /mnt/etc/
-            sudo chroot /mnt
-    elif [[ $drivevar = sata && $osvar = fedora && $encryptvar = yes ]]
-        then
-            sudo cryptsetup luksOpen /dev/$rootvar cryptdata
-            sudo lvscan
-            sudo vgchange -ay
-            sudo mount /dev/mapper/vgubuntu-root /mnt
-            sudo mount $efivar /mnt/boot/efi
-            for i in /dev /dev/pts /proc /sys /run; do sudo mount -B $i /mnt$i; done
-            sudo cp /etc/resolv.conf /mnt/etc/
-            sudo chroot /mnt
-    elif [[ $drivevar = nvme && $osvar = fedora && $encryptvar = no ]]
-        then
-            sudo mount $rootvar /mnt
-            sudo mount $efivar /mnt/boot/efi
-            for i in /dev /dev/pts /proc /sys /run; do sudo mount -B $i /mnt$i; done
-            sudo cp /etc/resolv.conf /mnt/etc/
-            sudo chroot /mnt
-    elif [[ $drivevar = sata && $osvar = fedora && $encryptvar = no ]]
-        then
-            sudo mount $rootvar /mnt
-            sudo mount $efivar /mnt/boot/efi
-            for i in /dev /dev/pts /proc /sys /run; do sudo mount -B $i /mnt$i; done
-            sudo cp /etc/resolv.conf /mnt/etc/
-            sudo chroot /mnt
+## Nobara section
 
-### Nobara section
-
-    elif [[ $drivevar = nvme && $osvar = nobara && $encryptvar = yes ]]
-        then
-            sudo cryptsetup luksOpen $rootvar cryptdata
-            sudo lvscan
-            sudo vgchange -ay
-            sudo mount /dev/mapper/nobara_localhost--live-root /mnt
-            sudo mount /dev/mapper/nobara_localhost--live-home /mnt/home
-            sudo mount $efivar /mnt/boot/efi
-            for i in /dev /dev/pts /proc /sys /run; do sudo mount -B $i /mnt$i; done
-            sudo cp /etc/resolv.conf /mnt/etc/
-            sudo chroot /mnt
-    elif [[ $drivevar = sata && $osvar = nobara && $encryptvar = yes ]]
-        then
-            sudo cryptsetup luksOpen /dev/$rootvar cryptdata
-            sudo lvscan
-            sudo vgchange -ay
-            sudo mount /dev/mapper/nobara_localhost--live-root /mnt
-            sudo mount $efivar /mnt/boot/efi
-            for i in /dev /dev/pts /proc /sys /run; do sudo mount -B $i /mnt$i; done
-            sudo cp /etc/resolv.conf /mnt/etc/
-            sudo chroot /mnt
-    elif [[ $drivevar = nvme && $osvar = nobara && $encryptvar = no ]]
-        then
-            sudo mount $rootvar /mnt
-            sudo mount $efivar /mnt/boot/efi
-            for i in /dev /dev/pts /proc /sys /run; do sudo mount -B $i /mnt$i; done
-            sudo cp /etc/resolv.conf /mnt/etc/
-            sudo chroot /mnt
-    elif [[ $drivevar = sata && $osvar = nobara && $encryptvar = no ]]
-        then
-            sudo mount $rootvar /mnt
-            sudo mount $efivar /mnt/boot/efi
-            for i in /dev /dev/pts /proc /sys /run; do sudo mount -B $i /mnt$i; done
-            sudo cp /etc/resolv.conf /mnt/etc/
-            sudo chroot /mnt
-fi
+### sudo mount /dev/mapper/nobara_localhost--live-root /mnt
+### sudo mount /dev/mapper/nobara_localhost--live-home /mnt/home
 
